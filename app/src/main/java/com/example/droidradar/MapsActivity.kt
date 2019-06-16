e0418d01dd33d5196c6d6878edfbd50b9b356fa2
drew <you@example.com>
2019-06-15 23:00:33 -0300

    Circle range  now responsive to seekBar, plus circle radius shown in subtitle

--- app/src/main/java/com/example/droidradar/MapsActivity.kt
+++ app/src/main/java/com/example/droidradar/MapsActivity.kt
@@ -2,14 +2,21 @@ package com.example.droidradar
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
 import android.location.LocationManager
+import android.media.MediaFormat
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.TextView
 import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.*
@@ -25,33 +32,50 @@ import com.google.android.gms.location.LocationServices.getFusedLocationProvider
 import com.google.android.gms.maps.model.*
 
 import com.google.android.gms.maps.model.CameraPosition
+import kotlinx.android.synthetic.main.activity_maps.*
 
 
 
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, SeekBar.OnSeekBarChangeListener {
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var markerOptions: MarkerOptions
     private var locationRequest: LocationRequest? = null
     private lateinit var mMap: GoogleMap
-    //private lateinit var local: Location
+    private lateinit var seekBar: SeekBar
     lateinit var pre : Marker
+    lateinit var circle: Circle
+    lateinit var circleLatLng: LatLng
     var fusedLocationClient: FusedLocationProviderClient? = null
-
+    private lateinit var distance : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
         locationRequest = LocationRequest.create();
         locationRequest?.priority=(LocationRequest.PRIORITY_HIGH_ACCURACY)
         locationRequest?.interval=(30*1000)
         //locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
         fusedLocationClient = getFusedLocationProviderClient(this)
+        seekBar = findViewById(R.id.seekBar)
+        seekBar.progress = 3
+        distance = seekBar.progress.toString()
+        supportActionBar?.subtitle = distance + "km"
+
+        seekBar.setOnSeekBarChangeListener(this)
+
+        ///############################################################//
+
+
+
 
         if (checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION)) {
@@ -69,12 +93,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                         dialog.show()
                     }
                     if (location != null) {
-                        val pos_latitude = location.latitude
-                        val pos_longitude = location.longitude
+                        val posLatitude = location.latitude
+                        val posLongitude = location.longitude
                         pre = mMap.addMarker(markerOptions
-                            .position(LatLng(pos_latitude,pos_longitude))
+                            .position(LatLng(posLatitude,posLongitude))
                             .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
-
+                        circle = mMap.addCircle(CircleOptions()
+                             .center(LatLng(posLatitude,posLongitude))
+                             .radius(seekBar.progress.toDouble()*1000)
+                             .strokeColor(Color.BLUE)
+                        )
                     }
 
 
@@ -92,23 +120,29 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
                         if (location != null) {
 
-                            val pos_latitude = lr.locations.last().latitude
-                            val pos_longitude = lr.locations.last().longitude
+                            val posLatitude = lr.locations.last().latitude
+                            val posLongitude = lr.locations.last().longitude
                             val stringLocation = location.toString()
+                            circleLatLng = LatLng(posLatitude,posLongitude)
                             pre.remove()
+                            circle.remove()
                             pre = mMap.addMarker(markerOptions
-                                    .position(LatLng(pos_latitude,pos_longitude))
+                                    .position(LatLng(posLatitude,posLongitude))
                                     .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
                             )
-
+                            circle = mMap.addCircle(CircleOptions()
+                                .center(LatLng(posLatitude,posLongitude))
+                                .radius(seekBar.progress.toDouble()*1000)
+                                .strokeColor(Color.BLUE)
+                            )
                             val oldPos = mMap.cameraPosition
                             val pos = CameraPosition.builder(oldPos)
-                                .target(LatLng(pos_latitude,pos_longitude))
-                                .zoom(17f)
+                                .target(LatLng(posLatitude,posLongitude))
+                                .zoom(13f)
                                 .build()
                             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(pos))
 
-                            Log.e("atualizando", "Newest Location: ")
+                            Log.e("atualizando", "Newest Location: "+String.format(stringLocation))
 
 
 
@@ -129,6 +163,30 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         }
     }
+
+    //#########################################################
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean){
+        distance = seekBar.progress.toString()
+        circle.remove()
+        circle = mMap.addCircle(CircleOptions()
+            .center(circleLatLng)
+            .radius(seekBar.progress.toDouble()*1000)
+            .strokeColor(Color.BLUE)
+        )
+        supportActionBar?.subtitle = distance + "km"
+        //Log.e("atualizando", "Newest distance: "+distance)
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+    }
+
+
+
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
